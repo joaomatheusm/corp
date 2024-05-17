dd0418d604144ced22ad09f665a3b3c5678e08ef
João Matheus <138176279+joaomatheusm@users.noreply.github.com>
2024-05-16 22:49:17 -0300

    imagens alteradas

--- assets/js/product-page.js
+++ assets/js/product-page.js
@@ -32,3 +32,47 @@ minus.addEventListener('click', () => {
 });
 
 add.addEventListener('click', () => input.value++);
+
+const imgColCream = [...document.querySelector('.cream .img-col').children];
+const imgColGray = [...document.querySelector('.gray .img-col').children];
+const mainImg = document.querySelector('.main-img').firstChild.nextSibling;
+
+function changeSelectedImg(evt) {
+    if (evt.target.className !== 'selected') {
+        imgColCream.forEach((e) => e.classList.remove('selected'));
+        evt.target.classList.add('selected');
+
+        mainImg.src = evt.target.src;
+    }
+}
+
+imgColCream.forEach((el) => {
+    el.addEventListener('click', (evt) => changeSelectedImg(evt));
+});
+
+imgColGray.forEach((el) => {
+    el.addEventListener('click', (evt) => changeSelectedImg(evt));
+});
+
+const labelColor = [...document.querySelectorAll('.label-color')];
+const iconColor = [...document.querySelectorAll('.label-i')];
+const creamBox = document.querySelector('.cream');
+const grayBox = document.querySelector('.gray');
+
+function changeColorDisplay(evt) {
+    if(evt.target.classList.contains('label-cream') || evt.target.parentNode.classList.contains('label-cream')) {
+        creamBox.style.display = 'flex';
+        grayBox.style.display = 'none';
+    }
+
+    if(evt.target.classList.contains('label-gray') || evt.target.parentNode.classList.contains('label-gray')) {
+        creamBox.style.display = 'none';
+        grayBox.style.display = 'flex';
+    }
+}
+
+labelColor.forEach(el => {
+    el.addEventListener('click', evt => changeColorDisplay(evt));
+
+    el.firstChild.addEventListener('click', evt => changeColorDisplay(evt));
+});
