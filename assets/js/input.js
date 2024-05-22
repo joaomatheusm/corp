83a6dc4ba6225adb6a40f61102fb2c7863599016
João Matheus <138176279+joaomatheusm@users.noreply.github.com>
2024-05-22 14:00:08 -0300

    input fixed

--- assets/js/input.js
+++ assets/js/input.js
@@ -2,7 +2,7 @@ const inputButton = document.querySelector('#input-button');
 const searchInput = document.querySelector('#search');
 
 inputButton.addEventListener('click', () => {
-    if(searchInput.style.display === 'none') {
+    if(getComputedStyle(searchInput).display === 'none') {
         searchInput.style.display = 'block';
     } else {
         searchInput.style.display = 'none';
